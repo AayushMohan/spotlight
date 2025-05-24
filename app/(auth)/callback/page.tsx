52cfb579177d31b3614f57dd857a82e9fbdb8610
AayushMohan <aayushmohan1702@gmail.com>
2025-05-24 16:10:10 +0530

    feat: implement authentication flow with Clerk and add Prisma setup

---
+++ app/(auth)/callback/page.tsx
@@ -0,0 +1,8 @@
+// TODO: Implement the authentication callback page
+export const dynamic = "force-dynamic";
+
+const AuthCallbackPage = () => {
+  return <div>AuthCallbackPage</div>;
+};
+
+export default AuthCallbackPage;
---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignUp } from "@clerk/nextjs";
+
+const Signup = () => {
+  return (
+    <div>
+      <SignUp />
+    </div>
+  );
+};
+export default Signup;
