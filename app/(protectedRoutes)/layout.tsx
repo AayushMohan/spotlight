783a37164a1d9604e22ea4ccaf8cd26eb8b84364
AayushMohan <aayushmohan1702@gmail.com>
2025-07-15 19:04:37 +0530

    feat: enhance Header component to accept user prop; update layout title to Spotlight; fix import path for button utility

--- app/(protectedRoutes)/layout.tsx
+++ app/(protectedRoutes)/layout.tsx
@@ -20,7 +20,7 @@ const Layout = async ({ children }: Props) => {
 
       <div className="flex flex-col w-full h-screen overflow-auto px-4 scrollbar-hide container mx-auto">
         {/* Header */}
-        <Header />
+        <Header user={userExist.user} />
         {children}
       </div>
     </div>
--- components/ReusableComponent/LayoutComponents/Header.tsx
+++ components/ReusableComponent/LayoutComponents/Header.tsx
@@ -1,4 +1,41 @@
-const Header = () => {
-  return <div>Header</div>;
+"use client";
+import { Button } from "@/components/ui/button";
+import { User } from "@prisma/client";
+import { ArrowLeft, Zap } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
+import PurpleIcon from "../PurpleIcon";
+
+type Props = { user: User };
+
+// TODO: Stripe Subscription, Assistant
+const Header = ({ user }: Props) => {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  return (
+    <div className="w-full px-4 pt-10 sticky top-0 z-10 flex justify-between items-center flex-wrap gap-4 bg-background">
+      {pathname.includes("pipeline") ? (
+        <Button
+          className="bg-primary/10 border-border rounded-xl"
+          variant={"outline"}
+          onClick={() => router.push("/webinar")}
+        >
+          <ArrowLeft /> Back to Webinars
+        </Button>
+      ) : (
+        <div className="px-4 py-2 flex justify-center text-bold items-center rounded-xl bg-background border border-border text-primary capitalize">
+          {pathname.split("/")[1]}
+        </div>
+      )}
+
+      {/* TODO: Build Stripe subscription and create webinar button */}
+      <div className="flex gap-6 items-center flex-wrap">
+        <PurpleIcon>
+          <Zap className="h-4 w-4" />
+        </PurpleIcon>
+      </div>
+    </div>
+  );
 };
 export default Header;
