9bba727e262dc44ea71843d46c10474adaa6828f
AayushMohan <aayushmohan1702@gmail.com>
2025-07-17 17:27:12 +0530

    Add lightthumbnail.png image asset to the public directory

---
+++ app/(protectedRoutes)/home/_components/OnBoarding.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { CheckCircle } from "lucide-react";
+import { onBoardingSteps } from "@/lib/data";
+import Link from "next/link";
+
+const OnBoarding = () => {
+  return (
+    <div className="flex flex-col gap-1 items-start justify-start">
+      {onBoardingSteps.map((step, index) => (
+        <Link key={index} href={step.link} className="flex items-center gap-2 ">
+          <CheckCircle />
+          <p className="text-base text-foreground">{step.title}</p>
+        </Link>
+      ))}
+    </div>
+  );
+};
+export default OnBoarding;
